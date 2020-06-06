c6348610248606e5fa4a6eee5a01cadbf0bc1867
anshumantsagar <anshumantsagar@gmail.com>
2020-06-07 00:45:49 +0530

    axios interceptors & instance

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -13,7 +14,7 @@ class Blog extends Component {
         error: false
     }
     componentDidMount () {
-        axios.get('http://jsonplaceholder.typicode.com/posts')
+        axios.get(axios.baseURL + '/posts')
             .then(resposne => {
                 const posts = resposne.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
